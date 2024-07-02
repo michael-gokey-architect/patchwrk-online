33a396ca294ce16f598302cdc83350914d456c4d
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-02 17:24:00 -0400

    Refactor to Test Forest Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,46 +6,39 @@ module.exports = {
 
   plugins: [
     // require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
     // require('tailwindcss/aspect-ratio'),
-    // require('@tailwindcss/container-queries'),
+		// require('@tailwindcss/container-queries'),
+		createThemes({
+			'light': { 
+					'primary': 'steelblue',
+					'secondary': 'darkblue',
+					'base': '#F3F3F3',
+			},
+			'dark': { 
+					'primary': 'turquoise',
+					'secondary': 'tomato',
+					'base': '#4A4A4A',
+			},
+			'forest': {
+				'body': 'white',
+				'tertiary': '#FFFBEB',
+				'inactive': '#FEF3C7',
+				'secondary': '#B45309',
+				'accent-light': '#92400E',
+				'trim-light': '#92400E',
+				'accent-dark': '#14532D',
+				'primary': '#052E16',
+				'trim-dark': '#052E16',
+				'info': '#0A0A0A',
+				'success': '#22C55E',
+				'error': '#F97316',
+				'warning': '#DC2626',
+				}
+		})
   ],
-  variants: {},
-
   theme: {
-    // extend: {},
     extend: {
-      plugins: [
-        createThemes({
-          'light': { 
-             'primary': 'steelblue',
-             'secondary': 'darkblue',
-             'base': '#F3F3F3',
-          },
-          'dark': { 
-             'primary': 'turquoise',
-             'secondary': 'tomato',
-             'base': '#4A4A4A',
-          },
-          'forest': {
-            'body': 'White',
-            'tertiary'  : 'Amber 50',
-            'inactive': 'Amber 100',
-            'secondary': 'Amber 700',
-            'accent-light': 'Amber 800',
-            'trim-light': 'Amber 800',
-            'accent-dark': 'Green 900',
-            'primary': 'Green 950',
-            'trim-dark': 'Green 950',
-            'info': 'Netural 950',
-            'success': '22C55E',
-            'error': 'F97316',
-            'warning': 'DC2626',
-            }
-       }, {
-          produceThemeClass: (themeName) => `theme-${themeName}`
-       })
-      ],
       fontFamily: {
         'theme1': {
           sans: ['Graphik', 'sans-serif'],
