37e9f69fce6f4f7f4a66d61b71e609d08fce9941
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-02 18:53:40 -0400

    Update Classes - Retro Diner Based

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,92 +6,96 @@ module.exports = {
 
   plugins: [
     // require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
     // require('tailwindcss/aspect-ratio'),
-		// require('@tailwindcss/container-queries'),
-		createThemes({
-			'light': { 
-					'primary': 'steelblue',
-					'secondary': 'darkblue',
-					'base': '#F3F3F3',
-			},
-			'dark': { 
-					'primary': 'turquoise',
-					'secondary': 'tomato',
-					'base': '#4A4A4A',
-			},
-			'forest': {
-				'body': 'white',
-				'tertiary': '#FFFBEB',
-				'inactive': '#FEF3C7',
-				'secondary': '#B45309',
-				'accent-light': '#92400E',
-				'trim-light': '#92400E',
-				'accent-dark': '#14532D',
-				'primary': '#052E16',
-				'trim-dark': '#052E16',
-				'info': '#0A0A0A',
-				'success': '#22C55E',
-				'error': '#F97316',
-				'warning': '#DC2626',
-			},
-			'racers': {
-				'body': 'white',
-				'tertiary': '#FFFBEB',
-				'inactive': '#FEF3C7',
-				'secondary': '#B45309',
-				'accent-light': '#92400E',
-				'trim-light': '#92400E',
-				'accent-dark': '#14532D',
-				'primary': '#052E16',
-				'trim-dark': '#052E16',
-				'info': '#0A0A0A',
-				'success': '#22C55E',
-				'error': '#F97316',
-				'warning': '#DC2626',
-			},
-			'retro-diner': {
-				'body': 'white',
-				'tertiary': '#FFFBEB',
-				'inactive': '#FECDD3',
-				'secondary': '#F43F5E',
-				'accent-light': '#FB923C',
-				'trim-light': '#FB923C',
-				'accent-dark': '#0D9488',
-				'primary': '#451A03',
-				'trim-dark': '#451A03',
-				'info': '#0A0A0A',
-				'success': '#22C55E',
-				'error': '#F97316',
-				'warning': '#DC2626',
-			},
-		})
+    // require('@tailwindcss/container-queries'),
+    createThemes({
+      light: {
+        primary: "steelblue",
+        secondary: "darkblue",
+        base: "#F3F3F3",
+      },
+      dark: {
+        primary: "turquoise",
+        secondary: "tomato",
+        base: "#4A4A4A",
+      },
+      forest: {
+        body: "white",
+        tertiary: "#FFFBEB",
+        inactive: "#FEF3C7",
+        secondary: "#B45309",
+        "accent-light": "#92400E",
+        "trim-light": "#92400E",
+        "accent-dark": "#14532D",
+        primary: "#052E16",
+        "trim-dark": "#052E16",
+        info: "#0A0A0A",
+        success: "#22C55E",
+        error: "#F97316",
+        warning: "#DC2626",
+      },
+      racers: {
+        body: "white",
+        tertiary: "#FFFBEB",
+        inactive: "#FEF3C7",
+        secondary: "#B45309",
+        "accent-light": "#92400E",
+        "trim-light": "#92400E",
+        "accent-dark": "#14532D",
+        primary: "#052E16",
+        "trim-dark": "#052E16",
+        info: "#0A0A0A",
+        success: "#22C55E",
+        error: "#F97316",
+        warning: "#DC2626",
+      },
+      "retro-diner": {
+        body: "white",
+        tertiary: "#FFFBEB",
+        inactive: "#FECDD3",
+        secondary: "#F43F5E",
+        "accent-light": "#FB923C",
+        "trim-light": "#FB923C",
+        "accent-dark": "#0D9488",
+        primary: "#451A03",
+        "trim-dark": "#451A03",
+        info: "#0A0A0A",
+        success: "#22C55E",
+        error: "#F97316",
+        warning: "#DC2626",
+      },
+    }),
   ],
   theme: {
     extend: {
       fontFamily: {
-        'theme1': {
-          sans: ['Graphik', 'sans-serif'],
-          serif: ['Merriweather', 'serif'],
+        theme1: {
+          sans: ["Graphik", "sans-serif"],
+          serif: ["Merriweather", "serif"],
         },
-        'theme2': {
-          sans: ['Ropo', 'sans-serif'],
-          serif: ['Playfair', 'serif'],
+        theme2: {
+          sans: ["Ropo", "sans-serif"],
+          serif: ["Playfair", "serif"],
         },
-
       },
       extend: {
         spacing: {
-          '8xl': '96rem',
-          '9xl': '128rem',
+          "8xl": "96rem",
+          "9xl": "128rem",
         },
         borderRadius: {
-          '4xl': '2rem',
-        }
+          "4xl": "2rem",
+        },
       },
-    }
-  }
-}
+      borderWidth: {
+        1: "1px",
+        2: "2px",
+        3: "3px",
+      },
+    },
+  },
+};
 
 
 
